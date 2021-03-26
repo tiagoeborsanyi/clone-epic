04e2bd1642fa040da4b24ca014f4a84930273e59
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-26 13:34:54 -0300

    created navbar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
+import Logo from '../../assets/logo.png'
+import { IoIosGlobe, IoIosPerson } from 'react-icons/io'
+
 import './header.scss'
 
 const Header = () => (
   <div className='header'>
-    header
+    <img src={Logo} alt="Logo" className="header__logo"/>
+    <nav className="header__nav">
+      <a href="#" className="nav-link">
+        <span>store</span>
+      </a>
+      <a href="#" className="nav-link">perguntas frequentes</a>
+      <a href="#" className="nav-link">ajuda</a>
+    </nav>
+    <div className="menu">
+      <IoIosGlobe />
+      <a href="#" className="menu__link">
+        <IoIosPerson />
+        entrar
+      </a>
+      <button className="menu__buttom">baixar epic games</button>
+    </div>
   </div>
 )
 
