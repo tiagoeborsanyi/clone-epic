770b80c39954ea2b94426f50a3dd6bee4a864d63
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-28 16:31:31 -0300

    create carousel data

---
+++ src/components/carousel/carousel.js
@@ -0,0 +1,32 @@
+export const carouselData = [
+	{
+		id: 1,
+		card: 'card1',
+		image: 'url(https://lh3.googleusercontent.com/proxy/1c3_0wmkLLpKDSDfNVoJmVWfq6Sk3m92JWvV1JWp8XIgoeMFbh8LzPC6C_68I41DojqIvKp0MqMR3iPZdQm-oJFCF0b-mgN2iBl8FPwAiL4DsTHAczLRDeeiRk2_8dx_pM55A3jGnl2u9P7JOBqiWK22Kwlrw3Tm_XeeG2VbRfhA)'
+	},
+	{
+		id: 2,
+		card: 'card2',
+		image: 'url(https://lh3.googleusercontent.com/proxy/tP5BRCEPMu0T3cmse6BALw_dDwSwls60Qn4sqHA_W9z_ekQvDwaQIzhjk0degXE0GQrsDEPqY7cSvlEm3CqUEFDSz4z7vFLJ6aj6ud1byJYkX7qyg54m2vja)'
+	},
+	{
+		id: 3,
+		card: 'card3',
+		image: 'url(https://store-images.s-microsoft.com/image/apps.44193.64731639180234187.ef9f08c7-a053-4657-a057-02cbb6029516.9b0cdc89-43cd-4a11-92ab-6e8afa676012)'
+	},
+	{
+		id: 4,
+		card: 'card4',
+		image: 'url(https://image.api.playstation.com/vulcan/img/rnd/202103/1606/aH4k5p0BbyTpJsq9UDc9fXY8.jpg?w=440)'
+	},
+	{
+		id: 5,
+		card: 'card5',
+		image: 'url(https://image.api.playstation.com/vulcan/ap/rnd/202008/1115/qFuP40kxv5Hryu5sqcEKzTxb.png)'
+	},
+	{
+		id: 6,
+		card: 'card6',
+		image: 'url(https://image.winudf.com/v2/image/Y29tLmFwcGJ1aWxkZXIudTI5NDE5NTJwMzAyNTA2MF9zY3JlZW5fN18xNTIxOTI3NDM1XzA1MA/screen-7.jpg?fakeurl=1&type=.jpg)'
+	}
+]
