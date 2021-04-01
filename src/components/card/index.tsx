004fc757604c9ba024bf68104f30ffb120f735cd
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-01 20:23:03 -0300

    create simple component card

---
+++ src/components/card/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './card.scss'
+
+const Card = () => (
+  <div className='card'>
+    card
+  </div>
+)
+
+export default Card
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './homepage.scss'
 import Carousel from '../../components/carousel'
+import Card from '../../components/card'
 
 const Homepage = () => {
   return (
     <div>
       <Carousel />
+      <Card />
     </div>
   )
 }
