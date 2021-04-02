f26e7a44a0992217415fe1ee788a75a02ba72f72
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-02 19:59:11 -0300

    create component header-show-cards

---
+++ src/components/header-show-cards/index.tsx
@@ -0,0 +1,9 @@
+import './header-show-cards.scss'
+
+const HeaderShowCards = () => (
+  <div>
+    header
+  </div>
+)
+
+export default HeaderShowCards
--- src/components/show-cards/index.tsx
+++ src/components/show-cards/index.tsx
@@ -1,15 +1,20 @@
 import './show-cards.scss'
 
 import Card from '../card'
+import HeaderShowCards from '../header-show-cards'
 
 import {dataItems} from '../../dataItems'
 
 const ShowCards = () => (
   <div className='show-cards'>
+    <HeaderShowCards />
     {
       dataItems.map(card => (
         <>
-          <Card urlImg={card.urlImage} />
+          <Card 
+            key={card.id}
+            urlImg={card.urlImage} 
+          />
         </>
       ))
     }
