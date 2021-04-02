a623db07a096b7185ac225bc5c21dff53b78fdd0
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-01 21:03:24 -0300

    create card initial

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 import './card.scss'
+import Hitman from '../../assets/card-hitman.jpg'
 
 const Card = () => (
   <div className='card'>
-    card
+    <div className='card__img' style={{backgroundImage: `url(${Hitman})`}}></div>
+    <h3 className='card__title'>Hitman 3</h3>
+    <h4 className='card__subtitle'>IO Interactive</h4>
+    <span className='card__percent'>-25%</span>
+    <span className='card__old-value'>R$ 113,99</span>
+    <span className='card__value'>R$ 85,49</span>
   </div>
 )
 
--- src/components/carousel/carousel-item/index.tsx
+++ src/components/carousel/carousel-item/index.tsx
@@ -9,7 +9,7 @@ interface ICarouselItemProps {
 }
 
 const CarouselItem: React.FC<ICarouselItemProps> = ({ id, text, image, clicked, selectedImage }) => (
-  <div className={`img card img--${id} ${selectedImage === image ? 'selected': ''}` } onClick={() => clicked(id)}>
+  <div className={`img card-carousel img--${id} ${selectedImage === image ? 'selected': ''}` } onClick={() => clicked(id)}>
     <div className="img" style={{ backgroundImage: image }}></div>
     <span className="img__text">{ text }</span>
   </div>
