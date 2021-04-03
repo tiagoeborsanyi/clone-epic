80e6dd333c9859f73a06c7c3cbcf1671565e071a
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-02 21:19:39 -0300

    initial mini card speaker concept

--- src/components/show-columns-mini-cards/index.tsx
+++ src/components/show-columns-mini-cards/index.tsx
@@ -1,8 +1,15 @@
 import './show-columns-mini-cards.scss'
 
+import HeaderShowCards from '../header-show-cards'
+
 const ShowColumnsMiniCards = () => (
   <div className='show-columns-mini-cards'>
-    show columns mini cards
+    <HeaderShowCards title='Novos e populares' />
+    <div className='columns-mini-card'>
+      <div className='column-mini-card'></div>
+      <div className='column-mini-card'></div>
+      <div className='column-mini-card'></div>
+    </div>
   </div>
 )
 
