09e0fb80337de0513be11d0eebca2f761d3833d8
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-02 21:08:43 -0300

    create component showColumnsMiniCards

---
+++ src/components/show-columns-mini-cards/index.tsx
@@ -0,0 +1,9 @@
+import './show-columns-mini-cards.scss'
+
+const ShowColumnsMiniCards = () => (
+  <div className='show-columns-mini-cards'>
+    show columns mini cards
+  </div>
+)
+
+export default ShowColumnsMiniCards
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,12 +1,14 @@
 import './homepage.scss'
 import Carousel from '../../components/carousel'
 import ShowCards from '../../components/show-cards'
+import ShowColumnsMiniCards from '../../components/show-columns-mini-cards'
 
 const Homepage = () => {
   return (
     <div>
       <Carousel />
       <ShowCards />
+      <ShowColumnsMiniCards />
     </div>
   )
 }
