761b383a313c8e0ddb757b7bef148897784a8895
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-06 20:48:53 -0300

    created component boxGames

--- src/components/box-games/index.tsx
+++ src/components/box-games/index.tsx
@@ -1,8 +1,26 @@
 import './box-games.scss'
 
+import HeaderShowCards from '../header-show-cards'
+
 const BoxGames = () => (
-  <div>
-    BOX GAMES
+  <div className='box-games'>
+    <HeaderShowCards title='Jogos grátis' styleFont show />
+    <div className="box-games__cards">
+      <div className="box-games__card">
+        <div className="box-games__image" style={{backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/clone-epic.appspot.com/o/tales-of-the-neonsea.jpg?alt=media)'}}>
+          <span>GRÁTIS</span>
+        </div>
+        <h3 className='box-games__title'>Tales of the Neon Sea</h3>
+        <span className='box-games__subtitle'>Grátis - 08 de abr. às 12:00</span>
+      </div>
+      <div className="box-games__card">
+      <div className="box-games__image" style={{backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/clone-epic.appspot.com/o/3-out-of-ten.jpg?alt=media)'}}>
+          <span style={{backgroundColor: 'black'}}>EM BREVE</span>
+        </div>
+        <h3 className='box-games__title'>Tales of the Neon Sea</h3>
+        <span className='box-games__subtitle'>Grátis - 08 de abr. às 12:00</span>
+      </div>
+    </div>
   </div>
 )
 
