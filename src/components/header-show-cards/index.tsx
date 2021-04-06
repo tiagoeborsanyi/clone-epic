3c7ebeefac260ef75ecca5fa9abd04f43d4080c8
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-06 20:22:37 -0300

    add style on headerShowCards

--- src/components/header-show-cards/index.tsx
+++ src/components/header-show-cards/index.tsx
@@ -5,10 +5,11 @@ import { Link } from 'react-router-dom'
 interface IHeaderShowcardsProps {
   title: string
   show?: boolean
+  styleFont?: boolean
 }
  
-const HeaderShowCards: React.FC<IHeaderShowcardsProps> = ({ title, show }) => (
-  <div className='header-show-cards'>
+const HeaderShowCards: React.FC<IHeaderShowcardsProps> = ({ title, show, styleFont }) => (
+  <div className={`${styleFont && 'font_large'} header-show-cards`}>
     <h3>{title}</h3>
     <Link to='/' className={`${show ? 'header-show-cards__Show' : 'header-show-cards__invisible'}`}>VER TUDO</Link>
   </div>
