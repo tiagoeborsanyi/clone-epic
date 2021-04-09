0eb6bca9cfac7f7c2e3bd69dba42b1528d5301d8
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-09 19:28:24 -0300

    created simple compónent gamePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import './App.scss';
-import { Route } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import Header from './components/header'
 import Homepage from './pages/homepage'
+import GamePage from './pages/gamepage'
 
 function App() {
   return (
     <div className='container'>
       <Header />
       <div className='content'>
-      <Route exact path='/' component={Homepage} />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/game/:id' component={GamePage} />
+          <Redirect to='/' />
+        </Switch>
       </div>
       <footer className='footer'>FOOTER</footer>
     </div>
---
+++ src/pages/gamepage/index.tsx
@@ -0,0 +1,7 @@
+const GamePage = () => {
+  return (
+    <div>GAME PAGE</div>
+  )
+}
+
+export default GamePage
