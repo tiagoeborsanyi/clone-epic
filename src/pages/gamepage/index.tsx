80f7bd0b074094b6be07a46992df44babf89cf50
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-13 17:58:09 -0300

    work on fetch data gamePage component

--- src/pages/gamepage/index.tsx
+++ src/pages/gamepage/index.tsx
@@ -1,4 +1,19 @@
-const GamePage = () => {
+import { useEffect } from 'react'
+
+import { dataItems } from '../../dataItems'
+
+interface IGamePageProps {
+  match: any
+}
+
+const GamePage: React.FC<IGamePageProps> = ({match}) => {
+  console.log(match.params.id)
+
+  useEffect(() => {
+    // Se passar um id que nao existe (seja undefined) entao tenho que colocar uma modal de 404 ou page de 404
+    const filterData = dataItems.filter(item => item.id === match.params.id)
+    console.log(filterData[0])
+  }, [match.params.id])
   return (
     <div>GAME PAGE</div>
   )
