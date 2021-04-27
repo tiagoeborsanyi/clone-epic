d61095af0b1f3445c15e67adba65c4dd47d22663
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-27 16:29:39 -0300

    created legal footer section on component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -45,6 +45,11 @@ const Footer = () => (
         </div>
       </div>
     </div>
+    <div className='footer-legal'>
+      <p>
+        © 2021, Epic Games, Inc. Todos os direitos reservados. Epic, Epic Games, o logotipo da Epic Games, Fortnite, o logotipo do Fortnite, Unreal, Unreal Engine, o logotipo do Unreal Engine, Unreal Tournament e o logotipo do Unreal Tournament são marcas comerciais ou registradas da Epic Games, Inc. nos Estados Unidos da América e em outros lugares. Outras marcas e nomes de produtos são marcas registradas de seus respectivos donos. Transações para além dos EUA são feitas através da Epic Games International, S.à r.l.   
+      </p>
+    </div>
   </div>  
 )
 
