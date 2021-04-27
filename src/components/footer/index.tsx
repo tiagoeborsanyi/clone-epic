61f077b385aeae049b6cae6400a64f36876d6d4e
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-27 16:37:47 -0300

    finish footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,9 @@ import {
   AiFillFacebook,
   AiOutlineTwitter,
   IoLogoYoutube,
-  IoIosArrowUp
+  IoIosArrowUp,
+  SiEpicgames,
+  SiUnrealengine
 } from 'react-icons/all'
 
 import './footer.scss'
@@ -49,6 +51,17 @@ const Footer = () => (
       <p>
         © 2021, Epic Games, Inc. Todos os direitos reservados. Epic, Epic Games, o logotipo da Epic Games, Fortnite, o logotipo do Fortnite, Unreal, Unreal Engine, o logotipo do Unreal Engine, Unreal Tournament e o logotipo do Unreal Tournament são marcas comerciais ou registradas da Epic Games, Inc. nos Estados Unidos da América e em outros lugares. Outras marcas e nomes de produtos são marcas registradas de seus respectivos donos. Transações para além dos EUA são feitas através da Epic Games International, S.à r.l.   
       </p>
+    </div>
+    <div className='footer-politics'>
+      <ul className='footer-politics__items'>
+        <li className='footer-politics__item'>Termos de Serviço</li>
+        <li className='footer-politics__item'>Politica de Privacidade</li>
+        <li className='footer-politics__item'>Politica de Reembolso da Loja</li>
+      </ul>
+      <div className='footer-politics__arrows'>
+        <div className='footer-politics__arrow'><SiEpicgames /></div>
+        <div className='footer-politics__arrow'><SiUnrealengine /></div>
+      </div>
     </div>
   </div>  
 )
