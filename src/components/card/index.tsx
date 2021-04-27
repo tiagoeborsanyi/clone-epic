5b6073a079ee55a5452f8e1821379d39c1553286
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-26 22:23:37 -0300

    created simple function for added favorites game

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -9,23 +9,30 @@ interface ICardProps {
   otherProps: any
 }
 
-const Card: React.FC<ICardProps> = ({ urlImg, otherProps }) => (
-  <div className='card'>
-    <Link to={`/game/${otherProps.id}`} className='card__img-link'>
-      <div className='card__img' style={{backgroundImage: `url(${urlImg})`}} />
-      <button className='card__button'>
-        <BsPlusCircle />
-      </button>
-      <span className='card__tooltip'>
-        Para Lista de Desejos
-      </span>
-    </Link>
-    <h3 className='card__title'>{otherProps.title.slice(0, 23)} {otherProps.title.length > 22 && '...'}</h3>
-    <h4 className='card__subtitle'>{otherProps.subtitle}</h4>
-    <span className='card__percent'>{otherProps.discount}</span>
-    <span className='card__old-value'>{otherProps.oldValue}</span>
-    <span className='card__value'>{otherProps.newValue}</span>
-  </div>
-)
+const Card: React.FC<ICardProps> = ({ urlImg, otherProps }) => {
+  const handleAddGame = (event: any) => {
+    event.preventDefault()
+    console.log('clicado', otherProps.id)
+  }
+  
+  return (
+    <div className='card'>
+      <Link to={`/game/${otherProps.id}`} className='card__img-link'>
+        <div className='card__img' style={{backgroundImage: `url(${urlImg})`}} />
+        <button className='card__button' onClick={handleAddGame}>
+          <BsPlusCircle />
+        </button>
+        <span className='card__tooltip'>
+          Para Lista de Desejos
+        </span>
+      </Link>
+      <h3 className='card__title'>{otherProps.title.slice(0, 23)} {otherProps.title.length > 22 && '...'}</h3>
+      <h4 className='card__subtitle'>{otherProps.subtitle}</h4>
+      <span className='card__percent'>{otherProps.discount}</span>
+      <span className='card__old-value'>{otherProps.oldValue}</span>
+      <span className='card__value'>{otherProps.newValue}</span>
+    </div>
+  )
+}
 
 export default Card
