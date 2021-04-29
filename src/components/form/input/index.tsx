801f47f2e11be656e3d802a948755677a3ba7e84
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-29 12:32:37 -0300

    created component input

--- src/components/form/input/index.tsx
+++ src/components/form/input/index.tsx
@@ -1,8 +1,27 @@
+// import { InputHTMLAttributes } from 'react'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
 import './input.scss'
 
-const Input = () => (
+interface IInputValues {
+  required: boolean
+  handleInputChanged:  (event: React.FormEvent<HTMLInputElement>) => void
+  value: string
+}
+
+const Input: React.FC<IInputValues> = ({ required, handleInputChanged, value }) => (
   <div>
-    INPUT
+    <div className='signup-form__block'>
+      <input
+        type="text"
+        value={value}
+        placeholder='*Nome de exibição'
+        style={required ? {backgroundColor: 'rgb(85,67,67)'} : {}}
+        onChange={handleInputChanged}
+      />
+      texto {typeof required}
+      <AiOutlineInfoCircle className='form-svg' />
+      <span className='form-span' style={required ? {opacity: '1'} : {}}>necessário</span>
+    </div>
   </div>
 )
 
--- src/components/login/signup/index.tsx
+++ src/components/login/signup/index.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react'
-import { SiEpicgames, AiOutlineInfoCircle } from 'react-icons/all'
+import { SiEpicgames } from 'react-icons/si'
 
 import './signup.scss'
+import Input from '../../form/input'
 
 const SignUp = () => {
   const [name, setName] = useState('')
   const [required, setRequired] = useState(false)
 
   const handleInputChanged = (e: any) => {
+    console.log('teste')
     console.log(e.target.value, e.target.value.length)
     setName(e.target.value)
     if (e.target.value.length === 0) {
       setRequired(true)
+      console.log('verdade')
     } else {
       setRequired(false)
+      console.log('false')
     }
   }
   
@@ -22,17 +26,12 @@ const SignUp = () => {
       <SiEpicgames />
       <h3 className='signup-title'>cadastrar</h3>
       <form className='signup-form'>
-        <div className='signup-form__block'>
-          <input
-            type="text"
-            placeholder='*Nome de exibição'
-            value={name}
-            onChange={(e: React.FormEvent<HTMLInputElement>) => handleInputChanged(e)}
-            style={required ? {backgroundColor: 'rgb(85,67,67)'} : {}}
-          />
-          <AiOutlineInfoCircle className='form-svg' />
-          <span className='form-span' style={required ? {opacity: '1'} : {}}>necessário</span>
-        </div>
+        <Input 
+          value={name}
+          required={required}
+          handleInputChanged={handleInputChanged}
+        />
+        {typeof required}
       </form>
     </div>
   )
