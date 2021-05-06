dfe303f34d23c1bc591c3f1476cab17cc4427062
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-06 12:30:20 -0300

    add rotate card login on screen

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,9 +4,18 @@ import SignUp from '../../components/login/signup'
 
 const Login: React.FC = () => {
   return (
-    <div className='login'>
-      <SignUp />
-    </div>
+      <section className='login-section'>
+      <div className='login'>
+        <div className='login__side login__side--front'>
+          <SignUp />
+        </div>
+        <div className='login__side login__side--back'>
+          <div>
+            algumas coisas escritas
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
