dfdc9d7effdbda6cd28f2f6d15ba9de46c5bc13f
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-08 14:37:37 -0300

    created grid columns gor checkout page

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -16,26 +16,15 @@ const Modal: React.FC<IModalProps> = ({ show, modalClosed }) => {
           opacity: show ? '1' : '0'
         }}
       >
-        
-          <div className='metodo-pagamento__titulo'>método de pagamento</div>
-          <div className='metodo-pagamento__conteudo'>
-            <h3>escolher um método de pagamento</h3>
-            <div>
-              conteudo
-            </div>
+        <div className='modal-conteudo'></div>
+        <div className='modal-resumo'>
+          <div className='resume-title'>
+            <h3>resumo do pedido</h3>
           </div>
-
-
-        
-          <div className='finaliza-compra__titulo'>finalizar compra</div>
-        
-          <div className='resumo__titulo'>resumo pedido</div>
-          <div className='resumo__conteudo'>
-            <div className='resumo__conteudo--image' style={{backgroundImage: `url(https://firebasestorage.googleapis.com/v0/b/clone-epic.appspot.com/o/assassins-creed-valhala.webp?alt=media)`}}></div>
-            <div className='resumo__conteudo--texto'>
-              conteud de texto
-            </div>
+          <div className='resume-content'>
+            <div className='resume-content__image' style={{backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/clone-epic.appspot.com/o/snow-runner.webp?alt=media)'}}></div>
           </div>
+        </div>
       </div>
     </>
   )
