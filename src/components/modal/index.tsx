2bdf674dd199ed5bc2413b7c4ff08eff8579ab46
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-08 14:47:54 -0300

    work on values pageChackout

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -23,6 +23,37 @@ const Modal: React.FC<IModalProps> = ({ show, modalClosed }) => {
           </div>
           <div className='resume-content'>
             <div className='resume-content__image' style={{backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/clone-epic.appspot.com/o/snow-runner.webp?alt=media)'}}></div>
+            <div className='resume-content__values'>
+              <h4>Skat City</h4>
+              <h5>Por Snowman</h5>
+              <ul className='resume-content__items'>
+                <li className='resume-content__item'>
+                  <p>Preço listado</p>
+                  <span>R$ 28,99</span>
+                </li>
+                <li className='resume-content__item'>
+                  <p>Desconto</p>
+                  <span>- R$ 9,86</span>
+                </li>
+                <li className='resume-content__item'>
+                  <p>Preço</p>
+                  <span>R$ 19,13</span>
+                </li>
+              </ul>
+              <div></div>
+              <ul className='resume-content__items'>
+                <li className='resume-content__item'>
+                  <p>Total</p>
+                  <span>R$ 19,13</span>
+                </li>
+                <li className='resume-content__item'>
+                  <p>Você gasta</p>
+                  <span>R$ 19,13</span>
+                </li>
+              </ul>
+              <div></div>
+
+            </div>
           </div>
         </div>
       </div>
