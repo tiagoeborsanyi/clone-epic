24b1cbb0f5ca61a6293424679d20af2cd525d7cd
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-11 17:07:12 -0300

    fix view components logged

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import { Route, Switch, Redirect } from 'react-router-dom'
 
+import { useAuth } from './hooks/auth'
 import Header from './components/header'
 import Login from './pages/login'
 import Homepage from './pages/homepage'
@@ -9,17 +10,33 @@ import Biblioteca from './pages/biblioteca'
 import Footer from './components/footer'
 
 function App() {
+  const { logged } = useAuth()
+  let comps
+  if (!logged) {
+    comps = (
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/game/:id' component={GamePage} />
+        <Route path='/login' component={Login} />
+        <Redirect to='/' />
+      </Switch>
+    )
+  } else {
+    comps = (
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/game/:id' component={GamePage} />
+        <Route path='/biblioteca' component={Biblioteca} />
+        <Route path='/login' component={Login} />
+        <Redirect to='/' />
+      </Switch>
+    )
+  }
   return (
     <div className='container'>
       <Header />
       <div className='content'>
-        <Switch>
-          <Route exact path='/' component={Homepage} />
-          <Route path='/game/:id' component={GamePage} />
-          <Route path='/biblioteca' component={Biblioteca} />
-          <Route path='/login' component={Login} />
-          <Redirect to='/' />
-        </Switch>
+        {comps}
       </div>
       <Footer />
     </div>
