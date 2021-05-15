5211fe13349f83302f5ac3f9b3d1a68f61f08bbb
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-15 01:32:05 -0300

    add basic css on header-favorites

--- src/components/favorites/header-favorites/index.tsx
+++ src/components/favorites/header-favorites/index.tsx
@@ -4,15 +4,15 @@ import './header-favorites.scss'
 
 const HeaderFavorites: React.FC = () => (
   <div className='h-favorites'>
-    <ul>
-      <li><Link to='/'>Descobrir</Link></li>
-      <li><Link to='/'>Navegar</Link></li>
+    <ul className='h-favorites-list'>
+      <li className='h-favorites-list__item'><Link to='/'>Descobrir</Link></li>
+      <li className='h-favorites-list__item'><Link to='/'>Navegar</Link></li>
     </ul>
-    <div>
-      <p>Lista de desejos</p>
-      <span>2</span>
-      <form action="#">
-        <div>
+    <div className='h-favorites-bloco'>
+      <p className='h-favorites-bloco__title'>Lista de desejos</p>
+      <span className='h-favorites-bloco__number'>2</span>
+      <form action="#" className='h-favorites-bloco__form'>
+        <div className='h-favorites-bloco__form-input'>
           <input type="text" placeholder='Procurar' />
         </div>
       </form>
