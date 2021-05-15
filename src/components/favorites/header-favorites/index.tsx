a491b46213e9d22d0333f7412c518ee7d3679498
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-15 01:13:31 -0300

    add html on header-favorites

--- src/components/favorites/header-favorites/index.tsx
+++ src/components/favorites/header-favorites/index.tsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom'
+
 import './header-favorites.scss'
 
 const HeaderFavorites: React.FC = () => (
   <div className='h-favorites'>
+    <ul>
+      <li><Link to='/'>Descobrir</Link></li>
+      <li><Link to='/'>Navegar</Link></li>
+    </ul>
     <div>
-      drgfergerg
+      <p>Lista de desejos</p>
+      <span>2</span>
+      <form action="#">
+        <div>
+          <input type="text" placeholder='Procurar' />
+        </div>
+      </form>
     </div>
   </div>
 )
