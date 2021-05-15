95eb5bd44ddf1a3486ea3ee50a191e29075cd9a7
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-14 21:22:05 -0300

    add initial test

---
+++ src/components/box-games/box-games.test.ts
@@ -0,0 +1,9 @@
+// import React from "react";
+import {  } from "@testing-library/react";
+
+
+describe("<LoginForm />", () => {
+  test("should display a blank login form, with remember me checked by default", async () => {
+    // ???
+  });
+});
