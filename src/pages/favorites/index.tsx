75cb65197dac10d389a5143458a2107f54bca72e
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-15 01:59:22 -0300

    created notice on favorites

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -5,7 +5,11 @@ const Favorites: React.FC = () => {
   return (
     <div>
       <HeaderFavorites />
-      favoritos funfando
+      <div className='notice'>
+        <p>
+          Adicione itens à sua Lista de Desejos para permitir notificações por e-mail quando eles estiverem disponíveis, estiverem em promoção ou apresentarem novo conteúdo.
+        </p>
+      </div>
     </div>
   )
 }
